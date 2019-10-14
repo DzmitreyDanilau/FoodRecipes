32cfd22a7ed56f78412ebb6c0fdc0ec64a0958e7
DzmitreyDanilau <dzmitrey.danilau@gmail.com>
2019-10-14 22:31:01 +0300

    +error handle in ViewModel

--- app/src/main/java/by/dzmitrey/danilau/foodrecipies/viewmodels/RecipeListViewModel.kt
+++ app/src/main/java/by/dzmitrey/danilau/foodrecipies/viewmodels/RecipeListViewModel.kt
@@ -4,12 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import by.dzmitrey.danilau.foodrecipies.models.Recipe
 import by.dzmitrey.danilau.foodrecipies.repositories.RecipeListRepository
-import io.reactivex.Observable
-import io.reactivex.Observer
-import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import javax.inject.Inject
@@ -24,9 +19,10 @@ class RecipeListViewModel @Inject constructor(private val repository: RecipeList
             repository.searchRecipes(query, page)
                 .subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.io())
-                .subscribe {
-                    recipesList.postValue(it.recipesList)
-                })
+                .subscribe(
+                    { recipesList.postValue(it.recipesList) },
+                    { Timber.d("Error ViewModel: ${it.message}") })
+        )
     }
 
     fun getRecipes(query: String, page: Int) = repository.searchRecipes(query, page)
