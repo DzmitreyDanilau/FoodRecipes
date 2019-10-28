271f41517247da88bbee9752d5a65eff45597830
DmitryDanilov <d.danilov@vironit.co.uk>
2019-10-28 14:36:49 +0300

    >showProgressBar when fetching from Api

--- app/src/main/java/by/dzmitrey/danilau/foodrecipies/ui/activities/BaseActivity.kt
+++ app/src/main/java/by/dzmitrey/danilau/foodrecipies/ui/activities/BaseActivity.kt
@@ -1,22 +1,23 @@
 package by.dzmitrey.danilau.foodrecipies.ui.activities
 
-import android.view.View
-import android.widget.ProgressBar
 import by.dzmitrey.danilau.foodrecipies.R
+import by.dzmitrey.danilau.foodrecipies.adapters.OnRecipeListener
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_base.view.*
 
-abstract class BaseActivity : DaggerAppCompatActivity() {
-    lateinit var progressBar: ProgressBar
+abstract class BaseActivity : DaggerAppCompatActivity(), OnRecipeListener {
     override fun setContentView(layoutResID: Int) {
         val constraintLayout = layoutInflater.inflate(R.layout.activity_base, null)
         val frameLayout = constraintLayout.activity_content
-        progressBar = constraintLayout.progress_bar
         layoutInflater.inflate(layoutResID, frameLayout, true)
         super.setContentView(layoutResID)
     }
 
-    fun showProgressBar(visibility: Boolean) {
-        progressBar.visibility = if (visibility) View.VISIBLE else View.GONE
+    override fun onRecipeClick(position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCategoryClick(category: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- app/src/main/java/by/dzmitrey/danilau/foodrecipies/ui/activities/RecipeListActivity.kt
+++ app/src/main/java/by/dzmitrey/danilau/foodrecipies/ui/activities/RecipeListActivity.kt
@@ -1,16 +1,14 @@
 package by.dzmitrey.danilau.foodrecipies.ui.activities
 
-import android.content.Context
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import by.dzmitrey.danilau.foodrecipies.R
-import by.dzmitrey.danilau.foodrecipies.adapters.OnRecipeListener
 import by.dzmitrey.danilau.foodrecipies.adapters.RecipeRecyclerAdapter
 import by.dzmitrey.danilau.foodrecipies.models.backend.Recipe
 import by.dzmitrey.danilau.foodrecipies.viewmodels.RecipeListViewModel
@@ -19,16 +17,19 @@ import kotlinx.android.synthetic.main.activity_recipe_list.*
 import timber.log.Timber
 import javax.inject.Inject
 
-class RecipeListActivity : BaseActivity(), OnRecipeListener {
+class RecipeListActivity : BaseActivity() {
     @Inject
     lateinit var providerFactory: ViewModelProviderFactory
     lateinit var recipeListViewModel: RecipeListViewModel
     lateinit var recyclerView: RecyclerView
     lateinit var recipeListAdapter: RecipeRecyclerAdapter
+    lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_list)
+        progressBar = progress_bar
+        showProgressBar(true)
         recyclerView = recycler_view
         recipeListViewModel = ViewModelProvider(this, providerFactory)
             .get(RecipeListViewModel::class.java)
@@ -46,13 +47,14 @@ class RecipeListActivity : BaseActivity(), OnRecipeListener {
                 it?.let {
                     Timber.d("List of recipes: $it")
                     recipeListAdapter.setRecipes(it)
+                    showProgressBar(false)
                 }
 
             })
         recipeListViewModel.getRecipesListError().observe(this@RecipeListActivity,
-        Observer<String> {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
-        })
+            Observer<String> {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            })
     }
 
     private fun initRecyclerView() {
@@ -61,11 +63,7 @@ class RecipeListActivity : BaseActivity(), OnRecipeListener {
         recyclerView.layoutManager = LinearLayoutManager(this@RecipeListActivity)
     }
 
-    override fun onRecipeClick(position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onCategoryClick(category: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun showProgressBar(visibility: Boolean) {
+        progressBar.visibility = if (visibility) View.VISIBLE else View.GONE
     }
 }
