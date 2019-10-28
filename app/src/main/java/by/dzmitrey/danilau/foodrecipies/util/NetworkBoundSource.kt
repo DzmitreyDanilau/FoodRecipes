c8af183cb3b3a583325ab1e2c8738fca09cf8627
DzmitreyDanilau <dzmitrey.danilau@gmail.com>
2019-10-28 22:36:44 +0300

    +networkboundsource +ApiResponses

--- app/src/main/java/by/dzmitrey/danilau/foodrecipies/util/NetworkBoundSource.kt
+++ app/src/main/java/by/dzmitrey/danilau/foodrecipies/util/NetworkBoundSource.kt
@@ -8,13 +8,16 @@ import io.reactivex.functions.Function
 abstract class NetworkBoundSource<LocalType, RemoteType> {
 
     init {
+
     }
 
-    protected abstract fun getRemote(): Single<RemoteType?>?
+    protected abstract fun loadFromRemote(): Single<RemoteType?>?
 
-    protected abstract fun getLocal(): Flowable<RemoteType?>?
+    protected abstract fun shouldFetch(data: LocalType): Boolean
+
+    protected abstract fun loadfromDB(): Flowable<RemoteType?>?
 
     protected abstract fun saveCallResult(data: RemoteType?)
 
-    protected abstract fun mapper(): Function<LocalType, RemoteType>
+    protected abstract fun mapper(): Function<RemoteType, LocalType>
 }
